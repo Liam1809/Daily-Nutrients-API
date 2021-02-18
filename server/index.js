066abfb5e37be30f1c2fc479cf6284e7619a2277
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2021-02-18 15:27:13 +0000

    setting & connecting server to mongoDB

---
+++ server/index.js
@@ -0,0 +1,26 @@
+/* 
+    body-parser: enable send requests
+    cors: enable cors orgin requests
+    express: routing epxress framework
+    mongoose: creating models / connect mongoDB
+    nodemon: auto reset
+*/
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://Liam:18091999@cluster0.edpoc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false); //  no warnings in the console
