d295c2375fbfe722f638ba3c87a7dda9606e15c2
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2021-02-22 22:46:23 +0000

    adjust user model: remove name property

--- server/models/healthDetail.js
+++ server/models/healthDetail.js
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose';
 
 const healthDetailSchema = mongoose.Schema({
-    name: { type: String },
+    userID: { type: String },
     weight: { type: Number },
     height: { type: Number },
     sex: { type: String },
     age: { type: Number },
     bmr: { type: Number },
     bmi: { type: Number }
-    // bmr va bmi tinh trong controller 
 });
 
 export default mongoose.model('healthDetail', healthDetailSchema);
