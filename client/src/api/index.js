e917619221c129ef1dde107d7a840015bcceefbb
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2021-02-23 02:44:51 +0000

    implementing interceptors request to send token to server to verify authentication before any requests

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,11 +2,22 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'http://localhost:4000' });
 
+// send token back to server to verify authentication
+API.interceptors.request.use((req) => {
+    if (localStorage.getItem('userProfile')) {
+        req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('userProfile')).token}`;
+    }
+
+    return req;
+});
+
+// API REQUESTS to retrieve Health Details from server
 export const fetchHD = () => API.get('/HD');
 export const fetchHD_ID = (id) => API.get(`/HD/${id}`);
 export const createHD = (newHD) => API.post('/HD', newHD);
 export const updateHD = (id, updatedHD) => API.patch(`/HD/${id}`, updatedHD);
 export const deleteHD = (id) => API.delete(`/HD/${id}`);
 
+// API REQUESTS to retrieve Authentication from server
 export const signIn = (FormData) => API.post('/user/signin', FormData);
 export const signUp = (FormData) => API.post('/user/signup', FormData);
