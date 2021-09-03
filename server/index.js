0d347a6bd9bc20be04550be7146d2d2da4a81ded
Liam1809 <hkhanhlam99@gmail.com>
2021-09-03 18:56:46 +0100

    update

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 // import routers
 import healthDetailRouter from './routes/healthDetail.js';
@@ -16,6 +17,7 @@ import userRouter from './routes/user.js';
 import dietRouter from './routes/diet.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,10 +31,10 @@ app.use('/user', userRouter);
 app.use('/diet', dietRouter);
 
 // connect to mongoDB
-const CONNECTION_URL = 'mongodb+srv://Liam:18091999@cluster0.edpoc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://Liam:Liam1809@cluster0.ifm61.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 4000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
